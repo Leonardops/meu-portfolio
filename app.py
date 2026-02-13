b362357682f9893527f53c64141acdac81814c40
Leonardops <leonardovictorpires20@gmail.com>
2026-02-12 21:23:38 -0300

    renderização dinâmica de projetos com Flask

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projetos = [
+    {
+        "titulo": "Portfolio pessoal",
+        "descricao": "Site desenvolvido com flask, html e css.",
+        "link": "#"
+    },
+    {
+        "titulo": "Sistema em Python",
+        "descricao": "Projeto para praticar lógica e estrutura.",
+        "link": "#"
+    },
+    {
+        "titulo": "Projeto Futuro",
+        "descricao": "Em breve mais projetos aqui 👀",
+        "link": "#"
+    }
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", projetos=projetos)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
